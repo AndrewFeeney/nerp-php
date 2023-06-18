cac02089271a5bae68037cccaaf3be81fdedc9bf
Andrew Feeney <andrew@deskspacecreative.com.au>
2023-06-18 19:35:52 +1000

    Add a test for evaluating a simple addition

--- tests/Unit/EvaluatorTest.php
+++ tests/Unit/EvaluatorTest.php
@@ -2,6 +2,7 @@
 
 use Nerp\NodeTypes\Integer;
 use Nerp\Evaluator;
+use Nerp\NodeTypes\AddOperation;
 
 test('it_can_evaluate_an_integer', function () {
     $evaulator = new Evaluator();
@@ -11,4 +12,17 @@ test('it_can_evaluate_an_integer', function () {
     $result = $evaulator->evaluate($ast);
 
     expect($result)->toEqual(123);
+});
+
+test('it_can_evaluate_a_simple_addition', function () {
+    $evaulator = new Evaluator();
+
+    $ast = new AddOperation(
+        new Integer(1),
+        new Integer(2),
+    );
+
+    $result = $evaulator->evaluate($ast);
+
+    expect($result)->toEqual(3);
 });
